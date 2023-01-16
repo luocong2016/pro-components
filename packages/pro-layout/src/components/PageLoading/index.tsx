624adcd11d7df6cf0d55aa25a976986d1b76091c
luocong2016 <luocong2016@126.com>
2023-01-16 09:41:14 +0800

    refactor: 重构PageLoading
    重构PageLoading

--- packages/pro-layout/src/components/PageLoading/index.tsx
+++ packages/pro-layout/src/components/PageLoading/index.tsx
@@ -1,19 +1,19 @@
-import 'ant-design-vue/es/spin/style';
-
-import { defineComponent, h } from 'vue';
-import { Spin, type SpinProps } from 'ant-design-vue';
-import { spinProps } from 'ant-design-vue/es/spin/Spin';
+import { defineComponent } from "vue";
+import { Spin, type SpinProps } from "ant-design-vue";
 
 export type PageLoadingProps = SpinProps;
 
-const PageLoading = defineComponent({
-  name: 'PageLoading',
-  props: {
-    ...spinProps(),
-  },
-  render() {
-    return h('div', { style: { paddingTop: '100px', textAlign: 'center' } }, h(Spin, { ...this.$props }));
+export default defineComponent<SpinProps & any>({
+  setup(props) {
+    const { isLoading, pastDelay, timedOut, error, retry, ...reset } = props;
+
+    return () => (
+      <div style={{ paddingBlockStart: '100px', textAlign: "center" }}>
+        <Spin
+          size='large'
+          {...reset}
+        />
+      </div>
+    );
   },
 });
-
-export default PageLoading;
